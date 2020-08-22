b2bc1814d4614d39691c138fd530c9edb82491fc
Anthony Wong <antawck@gmail.com>
2020-08-22 09:52:05 +0800

    more commander data

--- commander.py
+++ commander.py
@@ -2,3 +2,5 @@ class Commander:
 	def __init__(self, name, details):
 		self.name = name
 		self.race = details["race"]
+		self.sc2cooplink = details["sc2cooplink"]
+		self.wikilink = details["wikilink"]
