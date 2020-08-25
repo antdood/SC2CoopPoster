8693dc8404328362f460ce42016437c7eb3ec4d1
Anthony Wong <antawck@gmail.com>
2020-08-25 11:29:50 +0800

    reddit shenanigans

---
+++ reddit_thingamabobs.py
@@ -0,0 +1,18 @@
+import praw
+from yaml import safe_load
+
+reddit = None
+
+def getRedditInstance():
+	global reddit
+
+	if(reddit):
+		return reddit
+
+	with open("credentials.yml") as file:
+		credentials = safe_load(file)["reddit"]
+
+	reddit = praw.Reddit(**credentials)
+	reddit.validate_on_submit = True
+
+	return reddit.subreddit("starcraft2coop")
