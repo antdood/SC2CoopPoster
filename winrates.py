7c3c99f3879a6b520d879bfc3d2bd3a8052b8256
Anthony Wong <antawck@gmail.com>
2020-09-16 04:30:21 +0800

    winrates no longer scrape

--- winrates.py
+++ winrates.py
@@ -1,63 +1,23 @@
 import re
 from urllib.request import urlopen
 
-# No endpoint to query for these stats, so let's do some messy scraping
+winrates = {}
 
-html = ""
-data = {}
+def getWinRateOf(commanderName):
+	return getPickAndWinRateOf(commanderName)[1]
 
-def getRelevantHtml():
-	global html
+def getPickRateOf(commanderName):
+	return getPickAndWinRateOf(commanderName)[0]
 
-	if(html):
-		return html
+def getPickAndWinRateOf(commanderName):
+	if(commanderName == "Han & Horner"): # Hope no more exceptions come later on :/
+		commanderName = "Horner"
 
-	url = "https://starcraft2coop.com/resources/stats"
+	if(commanderName in winrates.keys()):
+		return winrates[commanderName]
 
-	page = urlopen(url).read().decode("utf-8")
+	url = "https://starcraft2coop.com/scripts/endpoints/reddit.php?commander=" + commanderName
 
-	i = page.index("var ctx = $('#communityCommanderWinRateChart');")
-
-	html = page[i:i+750]
-
-	return html
-
-
-def getWinrateData():
-	global data
-
-	if(data):
-		return data
-
-	commanders = re.search("labels: \[(.+?)\]", getRelevantHtml()).group(1).replace("\"", "").split(",")
-
-	wins, losses = map(lambda x: x.split(","), re.findall("data: \[(.+?)\]", getRelevantHtml()))
-
-	wins = list(map(int, wins))
-	losses = list(map(int, losses))
-
-	for n, commander in enumerate(commanders):
-		if(commander == "Horner"): # Hope no more exceptions come later on :/
-			commander = "Han & Horner"
-		data[commander] = {"wins" : wins[n], "losses" : losses[n]}
-
-	return data
-
-
-def getWinRateOf(commander):
-	return getWinrateData()[commander]["wins"]/getTotalGameCount(commander) * 100
-
-def getPickRateOf(commander):
-	return getTotalGameCount(commander)/getTotalGameCount() * 100
-
-def getTotalGameCount(commander = None):
-	if(commander):
-		return getWinrateData()[commander]["wins"]+getWinrateData()[commander]["losses"]
-	else:
-		total = 0
-
-		for c in getWinrateData().values():
-			total += c["wins"]
-			total += c["losses"]
-
-		return total
+	# url returns '[<pickrate>, <winrate>]'
+	winrates[commanderName] = tuple(eval(urlopen(url).read()))
+	return winrates[commanderName]
